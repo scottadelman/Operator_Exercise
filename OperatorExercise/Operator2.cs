088ad7964bf32068f3295b2378f55cf762d08761
Scott <ScottAdelman97@gmail.com>
2022-07-06 10:45:32 -0600

    simple calculator

---
+++ OperatorExercise/Operator2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Operator2
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Difference(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Modulus(int x, int y)
+        {
+            return x % y;
+        }
+    }
+}
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter simple equation below");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int quotient = a / b;
+int remainder = a % b;
+
+Console.WriteLine("Your answer is... " + quotient + " with a remainder of " + remainder);
